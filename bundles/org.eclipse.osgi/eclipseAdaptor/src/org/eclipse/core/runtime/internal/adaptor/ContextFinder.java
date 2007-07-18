c94d280d7e1118766c0648a7c6ce20095b791a53
Thomas Watson <twatson>
2007-07-18 19:40:17 +0000

    Bug 195566 ContextFinder.loadClass should not by synchronous

--- bundles/org.eclipse.osgi/eclipseAdaptor/src/org/eclipse/core/runtime/internal/adaptor/ContextFinder.java
+++ bundles/org.eclipse.osgi/eclipseAdaptor/src/org/eclipse/core/runtime/internal/adaptor/ContextFinder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -108,7 +108,7 @@ public class ContextFinder extends ClassLoader implements PrivilegedAction {
 		((Set) cycleDetector.get()).remove(name);
 	}
 
-	protected synchronized Class loadClass(String arg0, boolean arg1) throws ClassNotFoundException {
+	protected Class loadClass(String arg0, boolean arg1) throws ClassNotFoundException {
 		//Shortcut cycle
 		if (startLoading(arg0) == false)
 			throw new ClassNotFoundException(arg0);
