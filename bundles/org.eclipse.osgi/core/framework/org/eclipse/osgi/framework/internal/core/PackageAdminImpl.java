483faa9f28ad144372cc48861254fa6d4b8240e5
Thomas Watson <twatson>
2009-12-15 22:11:04 +0000

    Merge content from HEAD (tag v20091215) to branch.

--- bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/PackageAdminImpl.java
+++ bundles/org.eclipse.osgi/core/framework/org/eclipse/osgi/framework/internal/core/PackageAdminImpl.java
@@ -424,7 +424,7 @@ public class PackageAdminImpl {
 		}
 		AbstractBundle[] refresh = (AbstractBundle[]) bundlesList.toArray(new AbstractBundle[bundlesList.size()]);
 		// first sort by id/start-level order
-		Util.sort(refresh);
+		Util.sort(refresh, 0, refresh.length);
 		// then sort by dependency order
 		StartLevelManager.sortByDependency(refresh, framework);
 		boolean[] previouslyResolved = new boolean[refresh.length];
