8a106ac643666e9262e3d01186ef55bc090e928b
Thomas Watson <twatson>
2011-05-05 15:55:17 +0000

    Bug 344855 - ClassCastException inside debug code prevents tests from being run in debug mode

--- bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/module/MappedList.java
+++ bundles/org.eclipse.osgi/resolver/src/org/eclipse/osgi/internal/module/MappedList.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2010 IBM Corporation and others.
+ * Copyright (c) 2005, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -75,7 +75,7 @@ public class MappedList {
 		while (iter.hasNext()) {
 			Object value = iter.next();
 			if (value.getClass().isArray()) {
-				Object[] values = (Object[]) iter.next();
+				Object[] values = (Object[]) value;
 				for (int i = 0; i < values.length; i++)
 					results.add(values[i]);
 			} else
